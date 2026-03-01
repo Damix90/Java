a473ca28cd1464812721991f2c0051cd2ecc7cd7
Damien Huber <167686302+Damix90@users.noreply.github.com>
2026-03-01 14:31:37 +0100

    Add ExempleFor class to print numbers from 1 to 5

---
+++ ExempleFor.java
@@ -0,0 +1,8 @@
+public class ExempleFor {
+    public static void main(String[] args) {
+        // Affiche les nombres de 1 à 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Nombre : " + i);
+        }
+    }
+}
